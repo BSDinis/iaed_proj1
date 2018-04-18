78ba4726bee8f1dcb45f806c56bb8d999e2df70c
Baltasar Dinis <baltasardinis@gmail.com>
2018-04-18 12:43:54 +0100

    Created the position datatype. Untested, but trivial

---
+++ pos.h
@@ -0,0 +1,32 @@
+/*
+ *
+ * pos.h
+ *
+ * Baltasar Dinis 89416
+ * IAED project
+ *
+ * position datatype header file
+ *
+ * note on the data abstraction: this datatype is simple a tuple with 2 
+ * entries that represents a position in the sparse matrix
+ *
+ * the entire program is written under the assumption that only matrices 
+ * will be handled.
+ *
+ * this means that the abstraction to a n-dimensional tensor will imply 
+ * several changes in the program
+ */
+
+typedef struct {
+  unsigned int i, j;
+} pos;
+
+/* selectors */
+#define line(a) (a.i)
+#define col(a) (b.j)
+
+/* equality */
+#define eq_pos(a, b) (a.i == b.i && a.j == b.j)
+
+/* constructor */
+pos init_pos(unsigned int i, unsigned int j);
